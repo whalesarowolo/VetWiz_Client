8b551b6185e3cb877cc3dfaf33a4ef30efb54106
Marvin Ambrose <marvambi@primal.com.ng>
2020-01-16 17:18:26 +0100

    Added form stubs to the farmers profile

--- js/router.js
+++ js/router.js
@@ -2,7 +2,7 @@
 // #region Global-Variables
 let pageUrls = {
     blog: '/index.html',
-    Services:'/index.html?Services',
+    Services: '/index.html?Services',
     Interests: '/index.html?Interests',
     Rants: '/index.html?Rants',
     About: '/index.html?About',
@@ -23,17 +23,17 @@ OnStartUp();
 
 // #region Navlinks-Routing
 document.querySelector('#blogLuncher').addEventListener('click', (event) => {
-   let stateObj = {
-       page: 'blog'
-   } 
-   document.title = 'Blog';
-   history.pushState(stateObj, "blog", "?Blog");
-   RenderBlogPage();
+    let stateObj = {
+        page: 'blog'
+    }
+    document.title = 'Blog';
+    history.pushState(stateObj, "blog", "?Blog");
+    RenderBlogPage();
 });
 
 document.querySelector('#interestsLuncher').addEventListener('click', (event) => {
     let stateObj = {
-        page : 'interests'
+        page: 'interests'
     }
     document.title = 'Interests';
     history.pushState(stateObj, "interests", "?Interests");
@@ -42,7 +42,7 @@ document.querySelector('#interestsLuncher').addEventListener('click', (event) =>
 
 document.querySelector('#rantsLuncher').addEventListener('click', (event) => {
     let stateObj = {
-        page : 'rants'
+        page: 'rants'
     }
     document.title = 'Rants';
     history.pushState(stateObj, "rants", "?Rants");
@@ -54,17 +54,28 @@ document.querySelector('#rantsLuncher').addEventListener('click', (event) => {
 // #region View-Renderers
 function RenderBlogPage(params) {
     document.querySelector('main').innerHTML =
-    `<div class="column is-4 main-features">
+        `<div class="column is-4">
+        <div class="field">
+            <label class="label pull-left is-large">Name</label>
+            <div class="control has-icons-left">
+                <input class="input is-large is-rounded has-icon-left" type="email" placeholder="Text input">
+                <span class="icon is-left">
+                    <i class="fa fa-envelope"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="column is-4">
         <form>
-        <div class="field is-left">
-            <label class="label">Name</label>
+        <div class="field">
+            <label class="label pull-left">Name</label>
             <div class="control">
             <input class="input" type="text" placeholder="Text input">
             </div>
         </div>
         
-        <div class="field is-left">
-            <label class="label">Username</label>
+        <div class="field">
+            <label class="label pull-left">Username</label>
             <div class="control has-icons-left has-icons-right">
             <input class="input is-success" type="text" placeholder="Text input" value="bulma">
             <span class="icon is-small is-left">
@@ -77,8 +88,8 @@ function RenderBlogPage(params) {
             <p class="help is-success">This username is available</p>
         </div>
         
-        <div class="field is-left">
-            <label class="label">Email</label>
+        <div class="field">
+            <label class="label pull-left">Email</label>
             <div class="control has-icons-left has-icons-right">
             <input class="input is-danger" type="email" placeholder="Email input" value="[email protected]">
             <span class="icon is-small is-left">
@@ -92,7 +103,7 @@ function RenderBlogPage(params) {
         </div>
         
         <div class="field">
-            <label class="label">Subject</label>
+            <label class="label pull-left">Subject</label>
             <div class="control">
             <div class="select">
                 <select>
@@ -104,7 +115,146 @@ function RenderBlogPage(params) {
         </div>
         
         <div class="field">
-            <label class="label">Message</label>
+            <label class="label pull-left">Message</label>
+            <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+            </div>
+        </div>
+        
+        <div class="field">
+            <div class="control">
+            <label class="checkbox">
+                <input type="checkbox">
+                I agree to the <a href="#">terms and conditions</a>
+            </label>
+            </div>
+        </div>
+        
+        <div class="field">
+            <div class="control">
+            <label class="radio">
+                <input type="radio" name="question">
+                Yes
+            </label>
+            <label class="radio">
+                <input type="radio" name="question">
+                No
+            </label>
+            </div>
+        </div>
+        
+        <div class="field is-grouped">
+            <div class="control">
+            <button class="button is-link">Submit</button>
+            </div>
+            <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+            </div>
+        </div>
+        </form>
+    </div>
+    <div class="column is-2">
+        <div class="field">
+            <label class="label pull-left is-large">Name</label>
+            <div class="control has-icons-left">
+                <input class="input is-large is-rounded has-icon-left" type="email" placeholder="Text input">
+                <span class="icon is-left">
+                    <i class="fa fa-envelope"></i>
+                </span>
+            </div>
+        </div>
+    </div>`;
+}
+
+function RenderServicesPage(params) {
+    document.querySelector('main').innerHTML =
+        `<div class="section main-features">
+        <div class="columns is-3 container">
+            <div class="column card features">
+            <img src="./img/sms-1.png"/>
+            <h1>Good Agricultural Practices (GAP)</h1>
+            <p class="">Instant SMS on farming best practice. For market actors, deliver your message to Farm Aid’s large database of profiled farmers, to build your products’ brand awareness and increase sales.</p>
+            </div>
+
+            <div class="column card features">
+            <img src="./img/weather-icon.png"/>
+            <h1>Weather Advisory Services</h1>
+            <p>Accurate weather information sourced from proven and reliable weather service provider to help smallholder farmer plan their their processes.</p>
+            </div>
+
+            <div class="column card features">
+            <img src="./img/animal health.png"/>
+            <h1>Animal Health & Livestock Production</h1>
+            <p>Paravets and Community Animal health workers (CAHW) can subscribe to access information on the diagnosis, treatment and vaccines for animal diseases and sicknesses.</p>
+            </div>
+        </div>
+    </div>`;
+}
+
+function RenderRantsPage(params) {
+    document.querySelector('main').innerHTML =
+        `<div class="column is-4 main-features">
+        <div class="field">
+            <label class="label pull-left is-large">Name</label>
+            <div class="control has-icons-left">
+                <input class="input is-large is-rounded has-icon-left" type="email" placeholder="Text input">
+                <span class="icon is-left">
+                    <i class="fa fa-envelope"></i>
+                </span>
+            </div>
+        </div>
+    </div>
+    <div class="column is-4 main-features">
+        <form>
+        <div class="field">
+            <label class="label pull-left">Name</label>
+            <div class="control">
+            <input class="input" type="text" placeholder="Text input">
+            </div>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Username</label>
+            <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+            <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+            </span>
+            </div>
+            <p class="help is-success">This username is available</p>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Email</label>
+            <div class="control has-icons-left has-icons-right">
+            <input class="input is-danger" type="email" placeholder="Email input" value="[email protected]">
+            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-exclamation-triangle"></i>
+            </span>
+            </div>
+            <p class="help is-danger">This email is invalid</p>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Subject</label>
+            <div class="control">
+            <div class="select">
+                <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+                </select>
+            </div>
+            </div>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Message</label>
             <div class="control">
             <textarea class="textarea" placeholder="Textarea"></textarea>
             </div>
@@ -144,76 +294,115 @@ function RenderBlogPage(params) {
     </div>`;
 }
 
-function RenderServicesPage(params) {
-    document.querySelector('main').innerHTML =
-    `<div class="section main-features">
-        <div class="columns is-3 container">
-            <div class="column card features">
-            <img src="./img/sms-1.png"/>
-            <h1>Good Agricultural Practices (GAP)</h1>
-            <p class="">Instant SMS on farming best practice. For market actors, deliver your message to Farm Aid’s large database of profiled farmers, to build your products’ brand awareness and increase sales.</p>
-            </div>
-
-            <div class="column card features">
-            <img src="./img/weather-icon.png"/>
-            <h1>Weather Advisory Services</h1>
-            <p>Accurate weather information sourced from proven and reliable weather service provider to help smallholder farmer plan their their processes.</p>
-            </div>
-
-            <div class="column card features">
-            <img src="./img/animal health.png"/>
-            <h1>Animal Health & Livestock Production</h1>
-            <p>Paravets and Community Animal health workers (CAHW) can subscribe to access information on the diagnosis, treatment and vaccines for animal diseases and sicknesses.</p>
-            </div>
-        </div>
-    </div>`;
-}
-
-function RenderRantsPage(params) {
-    document.querySelector('main').innerHTML =
-    `<div class="section main-features">
-        <div class="columns is-3 container">
-            <div class="column card features">
-            <img src="./img/sms-1.png"/>
-            <h1>Good Agricultural Practices (GAP)</h1>
-            <p class="">Instant SMS on farming best practice. For market actors, deliver your message to Farm Aid’s large database of profiled farmers, to build your products’ brand awareness and increase sales.</p>
-            </div>
-
-            <div class="column card features">
-            <img src="./img/weather-icon.png"/>
-            <h1>Weather Advisory Services</h1>
-            <p>Accurate weather information sourced from proven and reliable weather service provider to help smallholder farmer plan their their processes.</p>
-            </div>
-
-            <div class="column card features">
-            <img src="./img/animal health.png"/>
-            <h1>Animal Health & Livestock Production</h1>
-            <p>Paravets and Community Animal health workers (CAHW) can subscribe to access information on the diagnosis, treatment and vaccines for animal diseases and sicknesses.</p>
-            </div>
-        </div>
-    </div>`;
-}
-
 function RenderInterestsPage(params) {
-    document.querySelector('main').innerHTML = 
-    `<div class="section main-features">
-        <div class="columns is-3 container">
-            <div class="column card features">
-            <img src="./img/sms-1.png"/>
-            <h1>Good Agricultural Practices (GAP)</h1>
-            <p class="">Instant SMS on farming best practice. For market actors, deliver your message to Farm Aid’s large database of profiled farmers, to build your products’ brand awareness and increase sales.</p>
+    document.querySelector('main').innerHTML =
+    `<div class="column is-4">
+        <div class="field">
+            <label class="label pull-left is-large">Name</label>
+            <div class="control has-icons-left">
+                <input class="input is-large is-rounded has-icon-left" type="email" placeholder="Text input">
+                <span class="icon is-left">
+                    <i class="fa fa-envelope"></i>
+                </span>
             </div>
-
-            <div class="column card features">
-            <img src="./img/weather-icon.png"/>
-            <h1>Weather Advisory Services</h1>
-            <p>Accurate weather information sourced from proven and reliable weather service provider to help smallholder farmer plan their their processes.</p>
+        </div>
+    </div>
+    <div class="column is-4">
+        <form>
+        <div class="field">
+            <label class="label pull-left">Name</label>
+            <div class="control">
+            <input class="input" type="text" placeholder="Text input">
             </div>
-
-            <div class="column card features">
-            <img src="./img/animal health.png"/>
-            <h1>Animal Health & Livestock Production</h1>
-            <p>Paravets and Community Animal health workers (CAHW) can subscribe to access information on the diagnosis, treatment and vaccines for animal diseases and sicknesses.</p>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Username</label>
+            <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+            <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+            </span>
+            </div>
+            <p class="help is-success">This username is available</p>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Email</label>
+            <div class="control has-icons-left has-icons-right">
+            <input class="input is-danger" type="email" placeholder="Email input" value="[email protected]">
+            <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+                <i class="fas fa-exclamation-triangle"></i>
+            </span>
+            </div>
+            <p class="help is-danger">This email is invalid</p>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Subject</label>
+            <div class="control">
+            <div class="select">
+                <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+                </select>
+            </div>
+            </div>
+        </div>
+        
+        <div class="field">
+            <label class="label pull-left">Message</label>
+            <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+            </div>
+        </div>
+        
+        <div class="field">
+            <div class="control">
+            <label class="checkbox">
+                <input type="checkbox">
+                I agree to the <a href="#">terms and conditions</a>
+            </label>
+            </div>
+        </div>
+        
+        <div class="field">
+            <div class="control">
+            <label class="radio">
+                <input type="radio" name="question">
+                Yes
+            </label>
+            <label class="radio">
+                <input type="radio" name="question">
+                No
+            </label>
+            </div>
+        </div>
+        
+        <div class="field is-grouped">
+            <div class="control">
+            <button class="button is-link">Submit</button>
+            </div>
+            <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+            </div>
+        </div>
+        </form>
+    </div>
+    <div class="column is-2">
+        <div class="field">
+            <label class="label pull-left is-large">Name</label>
+            <div class="control has-icons-left">
+                <input class="input is-large is-rounded has-icon-left" type="email" placeholder="Text input">
+                <span class="icon is-left">
+                    <i class="fa fa-envelope"></i>
+                </span>
             </div>
         </div>
     </div>`;
@@ -238,13 +427,13 @@ function logout() {
 function popStateHandler(event) {
     loc = window.location.href.toString().split(window.location.host)[1];
 
-    if (loc === pageUrls.Services){
-       RenderServicesPage();
+    if (loc === pageUrls.Services) {
+        RenderServicesPage();
     }
-    if(loc === pageUrls.blog){
+    if (loc === pageUrls.blog) {
         RenderBlogPage();
     }
-    if(loc == pageUrls.interests){
+    if (loc == pageUrls.interests) {
         RenderInterestsPage();
     }
 }
