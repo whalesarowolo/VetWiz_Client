22b6c5624ce99fbd45bae4b3e6bb2dd44ee9aa38
MajemiteJames <majemiteokoro@gmail.com>
2020-03-26 14:35:49 +0100

    added more features to the users page

--- js/dash/js-dashboard.js
+++ js/dash/js-dashboard.js
@@ -607,45 +607,10 @@ function usersPage(params) {
                                         <div class="button">
                                             <i class="material-icons">more_horiz</i>
                                         </div>
-
-                                        <div class="dropdown-menu is-text-bigger" role="menu">
-                                            <div class="dropdown-content">
-                                                <a class="dropdown-item">
-                                                    <i class="sl sl-icon-eye"></i>
-                                                    <span>
-                                                        <span>View</span>
-                                                        <span>View contact details</span>
-                                                    </span>
-                                                </a>
-                                                <a href="#" class="dropdown-item">
-                                                    <i class="sl sl-icon-pencil"></i>
-                                                    <span>
-                                                        <span>Edit</span>
-                                                        <span>Edit contact details</span>
-                                                    </span>
-                                                </a>
-                                                <div class="dropdown-divider"></div>
-                                                <a href="#" class="dropdown-item">
-                                                    <i class="sl sl-icon-bell"></i>
-                                                    <span>
-                                                        <span>Notifications</span>
-                                                        <span>Manage contact notifications</span>
-                                                    </span>
-                                                </a>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item">
-                                                    <i class="sl sl-icon-trash"></i>
-                                                    <span>
-                                                        <span>Delete</span>
-                                                        <span>Delete this contact</span>
-                                                    </span>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         
+                        </div>
                         </div>
                         
                         </article>
