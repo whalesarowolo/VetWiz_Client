88cc4de3cf7dc103463639bf380f0d4dd5b13a00
Marvin Ambrose <marvambi@primal.com.ng>
2020-05-07 14:02:35 +0100

    Refactored and Improved the flow of user administration on NVRI dashboard update with proposed look and feel update II

--- js/luncher_nvri.js
+++ js/luncher_nvri.js
@@ -5,6 +5,9 @@
         var router = new Router([            
             new Route('admin_nvri', 'admin_nvri.html'),
             new Route('nvri_users', 'nvri_users.html'),
+            new Route('activities', 'activities.html'),
+            new Route('profile', 'profile.html'),
+            new Route('diag_queries', 'diag_queries.html'),
         ]);
     }
     init();
