c6139dfb080e8fffec46d4384344da3a5d3a9b32
MajemiteJames <majemiteokoro@gmail.com>
2020-05-13 09:28:17 +0100

    fixed adding prevention bug

--- js/dash/disease.js
+++ js/dash/disease.js
@@ -58,31 +58,31 @@
 // }
 
 // Create a new list item when clicking on the "Add" button
-// function animalPrevention() {
-//    var li = document.createElement("li");
-//    var inputValue = document.getElementById("myInpu_prevention").value;
-//    var t = document.createTextNode(inputValue);
-//    li.appendChild(t);
-//    if (inputValue === '') {
-//      alert("You must write something!");
-//    } else {
-//      document.getElementById("myUL-prevention").appendChild(li);
-//    }
-//    document.getElementById("myInpu_prevention").value = "";
+function animalPrevention() {
+   var li = document.createElement("li");
+   var inputValue = document.getElementById("myInpu_prevention").value;
+   var t = document.createTextNode(inputValue);
+   li.appendChild(t);
+   if (inputValue === '') {
+     alert("You must write something!");
+   } else {
+     document.getElementById("myUL-prevention").appendChild(li);
+   }
+   document.getElementById("myInpu_prevention").value = "";
  
-//    var span = document.createElement("SPAN");
-//    var txt = document.createTextNode("\u00D7");
-//    span.className = "close";
-//    span.appendChild(txt);
-//    li.appendChild(span);
+   var span = document.createElement("SPAN");
+   var txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   li.appendChild(span);
  
-//    for (i = 0; i < close.length; i++) {
-//      close[i].onclick = function() {
-//        var div = this.parentElement;
-//        div.style.display = "none";
-//      }
-//    }
-//  }
+   for (i = 0; i < close.length; i++) {
+     close[i].onclick = function() {
+       var div = this.parentElement;
+       div.style.display = "none";
+     }
+   }
+ }
 
 
 // Create a new list item when clicking on the "Add" button
