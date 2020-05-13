35ac987814a4054563262fcbf94b394fcfe59402
MajemiteJames <majemiteokoro@gmail.com>
2020-05-13 09:38:18 +0100

    fixed add animal asscaited input field and prevention input filed bug

--- js/dash/disease.js
+++ js/dash/disease.js
@@ -58,6 +58,34 @@
 // }
 
 // Create a new list item when clicking on the "Add" button
+function diseaseAnimal() {
+   var li = document.createElement("li");
+   var inputValue = document.getElementById("animal_myInput").value;
+   var t = document.createTextNode(inputValue);
+   li.appendChild(t);
+   if (inputValue === '') {
+     alert("You must write something!");
+   } else {
+     document.getElementById("animal_myU").appendChild(li);
+   }
+   document.getElementById("animal_myInput").value = "";
+ 
+   var span = document.createElement("SPAN");
+   var txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   li.appendChild(span);
+ 
+   for (i = 0; i < close.length; i++) {
+     close[i].onclick = function() {
+       var div = this.parentElement;
+       div.style.display = "none";
+     }
+   }
+ }
+
+
+// Create a new list item when clicking on the "Add" button
 function animalPrevention() {
    var li = document.createElement("li");
    var inputValue = document.getElementById("myInpu_prevention").value;
