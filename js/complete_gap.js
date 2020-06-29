8933321ac2278d33ba244a2b625a8855ef8a5523
MajemiteJames <majemiteokoro@gmail.com>
2020-06-29 13:23:38 +0100

    implemented the functionality for the input for tomatoes

---
+++ js/complete_gap.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    $('textarea').on("propertychange keyup input paste",
+
+    function () {
+        var limit = $(this).data("limit");
+        var remainingChars = limit - $(this).val().length;
+        if (remainingChars <= 0) {
+            $(this).val($(this).val().substring(0, limit));
+        }
+        $(this).next('span').text(remainingChars<=0?0 + " Character":remainingChars + " Characters Remaining");
+    });
+});
+
+// tomatoes GAP functioanlity 
+
+    //  input details ends
+$("#tomatoes_input_button").on('click', function(e) {
+    let seedSelection = $('#to_ssd').val();
+    let seedSource = $('#to_ss').val();
+    let seedFertilizer = $('#to_ssf').val();
+    let seedHerbicide = $('#to_sh').val();
+    let seedProtection = $('#to_cpd').val();
+    console.log(seedProtection)
+    //  validate empty input boxes
+    if(seedSource != null || seedFertilizer != null ||  seedHerbicide != null || seedProtection != null || seedSelection !== null) {
+      let url = 'https://farm-aid-backend.herokuapp.com/api/crops/input/5ef9bb54c50f0e0017df379f'
+      let token = localStorage.getItem('access_token');
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Authorization', token);
+      let input = {
+        "seedSelection": seedSource,
+        "seedSource": seedSource,
+        "fertilizer": seedFertilizer,
+        "herbicides": seedHerbicide,
+        "cropProtectionProduct": seedProtection
+      }
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(input),
+        headers
+      }).then(async (res) => { 
+        let resp = await res.json();
+        console.log(resp)
+      })
+    }
+  })
+
+//   hausa
+//  input details ends
+$("#tomatoes_input_button_hausa").on('click', function(e) {
+    let seedSelection = $('#to_ssd_hausa').val();
+    let seedSource = $('#to_ss_hausa').val();
+    let seedFertilizer = $('#to_ssf_hausa').val();
+    let seedHerbicide = $('#to_sh_hausa').val();
+    let seedProtection = $('#to_cpd_hausa').val();
+    console.log(seedProtection)
+    //  validate empty input boxes
+    if(seedSource != null || seedFertilizer != null ||  seedHerbicide != null || seedProtection != null || seedSelection !== null) {
+      let url = 'https://farm-aid-backend.herokuapp.com/api/crops/hausa_input/5ef9bb54c50f0e0017df379f'
+      let token = localStorage.getItem('access_token');
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Authorization', token);
+      let input = {
+        "seedSelection": seedSource,
+        "seedSource": seedSource,
+        "fertilizer": seedFertilizer,
+        "herbicides": seedHerbicide,
+        "cropProtectionProduct": seedProtection
+      }
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(input),
+        headers
+      }).then(async (res) => { 
+        let resp = await res.json();
+        console.log(resp)
+      })
+    }
+  })
+
+// tomatoes GAP functioanlity  ends
