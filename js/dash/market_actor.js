8c7b3259c9725cf32e6e266e2acafc8da4260e1f
Marvin Ambrose <marvambi@primal.com.ng>
2020-07-25 16:30:43 +0100

    fixed more bugs

--- js/dash/market_actor.js
+++ js/dash/market_actor.js
@@ -198,7 +198,8 @@ $("#send_messages").on('click', function(e) {
   
   var msg_content = $("#final_sms_message").html();
   var state_criteria = JSON.parse(localStorage.getItem('chosen_criteria'));
-  var msg_state = state_criteria.states.split(',');
+  var msg_state_c = state_criteria.states;
+  var msg_state = msg_state_c.split(',');
   var message_recipients_query_id = $(".message-content-preview #final_sms_message").attr('data-id');
   //var female_gender = $("#female").is(":checked");
   // var msg_gender = (male_gender)? "male": "";
