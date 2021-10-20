c3bf1a9904794b48bcf4ef9a7959c859e818e05b
Marvin Ambrose <marvambi@gmail.com>
2021-10-20 18:11:08 +0100

    refactor: Updated the redirection policy for authentication check for VetWiz Web

--- js/dash/js-common-nvri.js
+++ js/dash/js-common-nvri.js
@@ -4,29 +4,25 @@
 js pageloader file 
 ========================================================================== */
 
-$(document).ready(function($){
-    
-    "use strict";
+$(document).ready(function ($) {
+  "use strict";
 
-    //Page loader
-    if ($('.pageloader').length) {
-
-        $('.pageloader').toggleClass('is-active');
-        const logged_id = localStorage.getItem('access_token');
-        if (logged_id == undefined || logged_id == null) {
-            console.log('You are not authorised...' + window.origin);
-            if (window.location.href !== window.origin + "/nvri-login.html") {
-                console.log('Already here...');
-                window.location.replace("/nvri-login.html");
-            }
-        }
-
-        $(window).on('load', function() {
-        var pageloaderTimeout = setTimeout( function() {
-            $('.pageloader').toggleClass('is-active');
-            $('.infraloader').toggleClass('is-active')
-            clearTimeout( pageloaderTimeout );
-        }, 700 );
-        })
+  //Page loader
+  if ($(".pageloader").length) {
+    $(".pageloader").toggleClass("is-active");
+    const logged_id = localStorage.getItem("access_token");
+    if (logged_id == undefined || logged_id == null) {
+      if (window.location.href !== window.origin + "/admin-login.html") {
+        window.location.replace("/admin-login.html");
+      }
     }
+
+    $(window).on("load", function () {
+      var pageloaderTimeout = setTimeout(function () {
+        $(".pageloader").toggleClass("is-active");
+        $(".infraloader").toggleClass("is-active");
+        clearTimeout(pageloaderTimeout);
+      }, 700);
+    });
+  }
 });
